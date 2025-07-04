5ee4fc71b41f2037cb7fd6385b9107a8618e8f84
Rane <2407071@cognizant.com>
2025-07-04 14:15:37 +0530

    fix: update com.library.gateway.config.SecurityConfig.java for CORS Filters and config in yml

--- api-gateway/src/main/java/com/library/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/library/gateway/config/SecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,5 +22,19 @@ public class SecurityConfig {
                 .anyExchange().permitAll()
             )
             .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedOrigin("http://localhost:5173");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 }
